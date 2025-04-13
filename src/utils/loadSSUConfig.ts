3952fcb6b21a94f6cfc2b54cb3c1796c6f676472
Mauricio León <mauleontejeda3@gmail.com>
2025-04-13 17:27:44 -0600

    Only use js files to configuration

--- src/utils/loadSSUConfig.ts
+++ src/utils/loadSSUConfig.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export async function loadUserConfig() {
-  const filenames = ['ssu.config.ts', 'ssu.config.js', 'ssu.config.mjs', 'ssu.config.cjs', 'ssu.config.json'];
+  const filenames = ['ssu.config.js', 'ssu.config.cjs']; // TODO: Pending support to Typescript config file 'ssu.config.ts'
 
   for (const filename of filenames) {
     const fullPath = path.resolve(process.cwd(), filename);
