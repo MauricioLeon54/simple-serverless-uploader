0da03a97a1b12d9c92ce413b2bc2ba37e0b5e63e
Mauricio León <mauleontejeda3@gmail.com>
2025-04-13 17:37:30 -0600

    add initial cli information to the project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+import { APP_VERSION } from 'src/config/appVersion';
+import { loadUserConfig } from 'src/utils/loadSSUConfig';
 
-console.log('Hello! Im the pacakge')
+async function main() {
+  const program = new Command();
+
+  const userConfig = await loadUserConfig();
+
+  program.name('ssu').description('Simple Serverless Upload CLI').version(APP_VERSION);
+
+  program.parse(process.argv);
+}
+
+main();
