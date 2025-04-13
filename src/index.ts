ca176d8def961a6195c40450a7ee5d8336b29bd6
Mauricio León <mauleontejeda3@gmail.com>
2025-04-13 17:50:53 -0600

    using types and call to deployProject function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import { APP_VERSION } from 'src/config/appVersion';
+import type { DeploymentType } from 'src/constants/DeploymentType';
+import { DeploymentTypes } from 'src/constants/DeploymentType';
+import { deployProject } from 'src/deployProject';
 import { loadUserConfig } from 'src/utils/loadSSUConfig';
 
 async function main() {
@@ -9,6 +12,14 @@ async function main() {
   const userConfig = await loadUserConfig();
 
   program.name('ssu').description('Simple Serverless Upload CLI').version(APP_VERSION);
+
+  program
+    .command('deploy <type>')
+    .description(`Deploy to AWS (${DeploymentTypes.S3_WEBSITE} or ${DeploymentTypes.LAMBDA_SERVER})`)
+    .option('--stage <stage>', `Deployment stage [${userConfig.stages.join(',')}]`)
+    .action(async (type: DeploymentType, { stage }: { stage: string }) => {
+      await deployProject({ type, stage });
+    });
 
   program.parse(process.argv);
 }
